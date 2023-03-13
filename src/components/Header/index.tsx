b02e59b43bfb83a7a30817b82437f46bebee1fae
Mário Santos <mariodev7@gmail.com>
2023-03-12 21:14:25 -0300

    feat(header): display badge when cart have items

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import { Handbag } from 'phosphor-react'
+import { useCart } from 'react-use-cart'
 import { HeaderWrapper, IconButton } from './styles'
 
 export const Header = () => {
+  const { totalItems } = useCart()
+
   return (
     <HeaderWrapper>
       <Image src="/logo.svg" width="130" height="52" alt="" />
 
-      <IconButton>
+      <IconButton hasItems={Boolean(totalItems)}>
         <Handbag fontWeight="bold" size={24} />
       </IconButton>
     </HeaderWrapper>
