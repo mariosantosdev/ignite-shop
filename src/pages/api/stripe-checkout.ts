7cbaa6d1b38ae69e0243549e88faf79f77b1bc30
Mário Santos <mariodev7@gmail.com>
2023-03-12 23:03:42 -0300

    feat(api): receiving products as list

--- src/pages/api/stripe-checkout.ts
+++ src/pages/api/stripe-checkout.ts
@@ -1,20 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { stripe } from '~/services/stripe'
 
+type Products = Array<{
+  priceId: string
+  quantity: number
+}>
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const priceId = req.body.priceId
+  const products = req.body.products as Products
 
-  if (!priceId) return res.status(400).json({ error: 'Price ID is required' })
+  if (!products || products.length === 0)
+    return res.status(400).json({ error: 'Products is required' })
 
   if (req.method !== 'POST')
     return res.status(405).json({ error: 'Method not allowed' })
 
   const checkoutSession = await stripe.checkout.sessions.create({
     mode: 'payment',
-    line_items: [{ price: priceId, quantity: 1 }],
+    line_items: products.map(({ priceId, quantity }) => ({
+      price: priceId,
+      quantity,
+    })),
     success_url: `${process.env.NEXT_PUBLIC_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${process.env.NEXT_PUBLIC_URL}/`,
   })
