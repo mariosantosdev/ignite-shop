bcf7bf2ebde38254d264aaee96123432ddb79573
Mário Santos <mariodev7@gmail.com>
2023-03-12 21:12:54 -0300

    feat(header): add cart button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
-import { HeaderWrapper } from './styles'
+import { Handbag } from 'phosphor-react'
+import { HeaderWrapper, IconButton } from './styles'
 
 export const Header = () => {
   return (
     <HeaderWrapper>
       <Image src="/logo.svg" width="130" height="52" alt="" />
+
+      <IconButton>
+        <Handbag fontWeight="bold" size={24} />
+      </IconButton>
     </HeaderWrapper>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,48 @@
 import { styled } from '~/styles'
 
 export const HeaderWrapper = styled('header', {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+
   padding: '2rem 0',
   width: '100%',
   maxWidth: '74rem',
   margin: '0 auto',
+})
+
+export const IconButton = styled('button', {
+  cursor: 'pointer',
+  position: 'relative',
+  border: 0,
+  borderRadius: 6,
+  padding: '1rem',
+  backgroundColor: '$gray800',
+
+  svg: {
+    color: '$gray400',
+  },
+
+  variants: {
+    hasItems: {
+      true: {
+        svg: {
+          color: '$white',
+        },
+        '&::after': {
+          content: '""',
+          color: '$white',
+          position: 'absolute',
+          top: '-.5rem',
+          right: '-.5rem',
+          border: '5px solid $gray900',
+          width: '1rem',
+          height: '1rem',
+          borderRadius: '50%',
+          backgroundColor: '$green300',
+        },
+      },
+    },
+  },
 })
